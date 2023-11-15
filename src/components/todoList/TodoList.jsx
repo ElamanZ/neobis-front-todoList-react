439caa740ab6b254b3bbb0307e36013670e15b0f
ElamanZ <z.elaman699@gmail.com>
2023-11-15 23:30:54 +0600

    Done delete and edit functionality

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,38 +1,77 @@
-import React from 'react';
-import '../todoList/todoList.css'
-function TodoList({todo, deleteTodo, onChangeTodoStatus}) {
+import React, { useState } from 'react';
+import '../todoList/todoList.css';
 
-    const handleCheckboxChange = () => {
-        onChangeTodoStatus(todo.id);
-    };
+function TodoList({ todo, deleteTodo, completeTodo }) {
+    const [editableText, setEditableText] = useState(todo.text);
+    const [isEditing, setIsEditing] = useState(false);
+
     const btnCompleted = () => {
-        return todo.completed = !todo.completed
-    }
+        completeTodo({
+            ...todo,
+            completed: !todo.completed,
+        });
+    };
+
+    const handleTextChange = (e) => {
+        setEditableText(e.target.value);
+    };
+
+    const editText = () => {
+        setIsEditing(!isEditing);
+    };
 
 
     return (
-        <>
-            <li className="list-group-item">
-                <div className="list-group-item-left">
-                    <label className="todo-list-radio">
-                        <input
-                            type="checkbox"
-                            onChange={handleCheckboxChange}
-                            checked={todo.completed}
-                            onClick={btnCompleted}
-                        />
-                        <span className={`radio-btn ${todo.category === 'personal' ? "personal" : "business"}`}></span>
-                    </label>
-                    <span id="editText" className="todo-task-text">{todo.text}</span>
-                </div>
-
-                <span className="todo-buttons">
-                    <button className="btn btn-edit-task">Edit</button>
-                    <button className="btn btn-edit-delete" onClick={()=> deleteTodo(todo)}>Delete</button>
-                </span>
-            </li>
-
-        </>
+        <li className="list-group-item">
+            <div className="list-group-item-left">
+                <label className="todo-list-radio">
+                    <input
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={btnCompleted}
+                    />
+                    <span
+                        className={`radio-btn ${
+                            todo.category === 'personal'
+                                ? 'personal'
+                                : 'business'
+                        }`}
+                    ></span>
+                </label>
+                {isEditing ? (
+                    <input
+                        className={`todo-task-text ${
+                            todo.completed ? 'task-done' : ''
+                        }`}
+                        value={editableText}
+                        onChange={handleTextChange}
+                    />
+                ) : (
+                    <span
+                        className={`todo-task-text ${
+                            todo.completed ? 'task-done' : ''
+                        } `}
+                        onDoubleClick={editText}
+                    >
+                        {editableText}
+                    </span>
+                )}
+            </div>
+            <span className="todo-buttons">
+                <button
+                    className="btn btn-edit-task"
+                    onClick={editText}
+                >
+                    {isEditing ? 'Done' : 'Edit'}
+                </button>
+                <button
+                    className="btn btn-edit-delete"
+                    onClick={() => deleteTodo(todo)}
+                >
+                    Delete
+                </button>
+            </span>
+        </li>
     );
 }
 
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,100 +1,113 @@
-import React, {useEffect, useState} from 'react';
-import '../mainPage/mainPage.css'
-import TodoList from "../../components/todoList/TodoList";
+import React, { useState } from 'react';
+import '../mainPage/mainPage.css';
+import TodoList from '../../components/todoList/TodoList';
 
 function MainPage() {
-    const [editableText, setEditableText] = useState("Elaman");
-    const [category, setCategory] = useState('personal')
-    const [completed, setCompleted] = useState(false)
-    const [textTodo, setTextTodo] = useState('')
-    const [id, setId] = useState(1)
+    const [editableText, setEditableText] = useState('Elaman');
+    const [category, setCategory] = useState('personal');
+    const [textTodo, setTextTodo] = useState('');
+    const [id, setId] = useState(1);
+    const [todos, setTodos] = useState([]);
 
-
-    const [todos, setTodos] = useState([])
     const handleCategoryChange = (event) => {
         setCategory(event.target.value);
     };
 
-
     const addNewTodo = () => {
-        if (textTodo !== "") {
+        if (textTodo !== '') {
             const newTodo = {
                 id: id,
                 text: textTodo,
-                completed: completed,
-                category: category === 'personal' ? 'personal' : 'business'
-            }
-            setTodos([...todos, newTodo])
-            setTextTodo('')
+                completed: false,
+                category: category === 'personal' ? 'personal' : 'business',
+            };
+            setTodos([...todos, newTodo]);
+            setTextTodo('');
             setId(id + 1);
-            console.log(todos)
         }
-    }
-    // useEffect(() => {
-    //     console.log(todos);
-    // }, [todos]);
+    };
 
-    const deleteBtn = (todo) => {
-    setTodos(todos.filter(t => t.id !== todo.id))
-    }
+    const deleteBtn = (todoToDelete) => {
+        setTodos(todos.filter((todo) => todo.id !== todoToDelete.id));
+    };
+
+    const completeTodo = (updatedTodo) => {
+        const updatedTodos = todos.map((todo) =>
+            todo.id === updatedTodo.id ? updatedTodo : todo
+        );
+        setTodos(updatedTodos);
+    };
+
     return (
-        <>
-            <div className="container">
-                    <h1 className="main__title">
-                        What's up,{' '}
-                        <span className="main__title edit">
-                            <input
-                                className="main__titleInput"
-                                type="text"
-                                value={editableText}
-                                onChange={(e) => setEditableText(e.target.value)}
-                            />
-                        </span>
-                    </h1>
-                    <p className="main__description">CREATE A TODO</p>
-
-                    <h3 className="main__todoText">What's on your todo list?</h3>
+        <div className="container">
+            <h1 className="main__title">
+                What's up,{' '}
+                <span className="main__title edit">
                     <input
-                        className="main__todoInput"
+                        className="main__titleInput"
                         type="text"
-                        placeholder="e.g. get a milk"
-                        value={textTodo}
-                        onChange={e => setTextTodo(e.target.value)}
+                        value={editableText}
+                        onChange={(e) => setEditableText(e.target.value)}
                     />
-                    <h3 className="main__todoText">What's on your todo list?</h3>
+                </span>
+            </h1>
+            <p className="main__description">CREATE A TODO</p>
 
-                    <div className="radio-buttons">
-                        <label className="radio-button">
-                            <input type="radio" name="category" value="business" onChange={handleCategoryChange} checked={category === 'business'}/>
-                            <span className="radio-btn business"></span>
-                            <div className="radio-title">Business</div>
-                        </label>
-                        <label className="radio-button">
-                            <input type="radio" name="category" value="personal" onChange={handleCategoryChange} checked={category === 'personal'}/>
-                            <span className="radio-btn personal"></span>
-                            <div className="radio-title">Personal</div>
-                        </label>
-                    </div>
+            <h3 className="main__todoText">What's on your todo list?</h3>
+            <input
+                className="main__todoInput"
+                type="text"
+                placeholder="e.g. get a milk"
+                value={textTodo}
+                onChange={(e) => setTextTodo(e.target.value)}
+            />
 
-                <div>
-                    <button className="btnAddTodo" onClick={addNewTodo}>Add todo</button>
-                </div>
-                {
-                    todos.length !== 0 ? (
-                        todos.map((todo) => (
-                            <TodoList
-                                todo={todo}
-                                key={todo.id}
-                                deleteTodo={deleteBtn}
-                            />
-                        ))
-                    ) : (
-                        <div className="main__todoText f-size">The task list is empty</div>
-                    )
-                }
-
+            <div className="radio-buttons">
+                <label className="radio-button">
+                    <input
+                        type="radio"
+                        name="category"
+                        value="business"
+                        onChange={handleCategoryChange}
+                        checked={category === 'business'}
+                    />
+                    <span className="radio-btn business"></span>
+                    <div className="radio-title">Business</div>
+                </label>
+                <label className="radio-button">
+                    <input
+                        type="radio"
+                        name="category"
+                        value="personal"
+                        onChange={handleCategoryChange}
+                        checked={category === 'personal'}
+                    />
+                    <span className="radio-btn personal"></span>
+                    <div className="radio-title">Personal</div>
+                </label>
             </div>
-        </>
+
+            <div>
+                <button className="btnAddTodo" onClick={addNewTodo}>
+                    Add todo
+                </button>
+            </div>
+
+            {todos.length !== 0 ? (
+                todos.map((todo) => (
+                    <TodoList
+                        todo={todo}
+                        key={todo.id}
+                        deleteTodo={deleteBtn}
+                        completeTodo={completeTodo}
+                    />
+                ))
+            ) : (
+                <div className="main__todoText f-size">
+                    The task list is empty
+                </div>
+            )}
+        </div>
     );
 }
 
